036f62bc425cb7acbc599bf47e5e95fe61505312
lkf <563421209@qq.com>
2018-03-23 19:51:41 +0800

    ofo

---
+++ REMADE.md
@@ -0,0 +1,17 @@
+ofo小程序部分功能
+=====================
+仿写ofo小程序部分功能，以微信map组件为核心写了首页，简单实现定位，扫码，报障，用户等功能。
+
+- 首页使用wx.getSystemInfo方法可以获得当前定位经纬度。进而定位到自己的位置。
+- 立即用车方面扫码获得的数据我使用了假数据网站easymock制造假数据，直接替代扫码获得的数据传入信息显示页面。
+- 报障页面使用wx:for渲染数据，使用微信icon图标来展现。填写信息input框使用h5的placeholder来聚焦失焦，相比于原来的onfocus、onblur更好使用。
+- 用户登陆界面则使用wx.getuserInfo将获取到的用户信息展示出来。
+- 用户充值方面由于没有使用服务器和后台接口，直接将充值数据传入Storage里，在钱包页面在获取。
+
+![view1](./images/view.jpg "view1")
+![view2](./images/view.png "view2")
+![view3](./images/view.png "view3")
+![view4](./images/view.png "view4")
+![view5](./images/view.png "view5")
+![view6](./images/view.png "view6")
+![view7](./images/view.png "view7")
--- projects/warn/warn.js
+++ projects/warn/warn.js
@@ -39,26 +39,12 @@ Page({
     checkedValue:[]
   },
   changeNum: function(e) {
-    // bindchangeNum = function(e) {
       this.setData({
         bikeInfo: {
           num: e.detail.value,
           desc: this.data.bikeInfo.desc
         }
       })
-    //   console.log(this.data.bikeInfo);
-    // };
-    // throttle = function(callback) {
-    //   var timer;
-    //   return function () {
-    //     clearTimeout(timer);
-    //     var that = this;
-    //     timer = setTimeout(function () {
-    //       callback.call(that);
-    //     }, 1000);
-    //   }
-    // };
-    // throttle(bindchangeNum(e));
   },
   changeDesc: function (e) {
     this.setData({
@@ -120,60 +106,5 @@ Page({
         }
       })
     }
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
